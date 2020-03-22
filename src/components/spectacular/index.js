23516ceaa46b3c434b3d341045cee6b3e135689a
NaChen <kiffom@sina.com>
2020-03-22 22:58:40 +0800

    cyy:初始化油库系统

---
+++ src/components/spectacular/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.less';
+
+/**
+ * 测试用
+ */
+class Spectacular extends React.PureComponent {
+
+  render() {
+    return <div><h1 className="testStyle">看板</h1></div>;
+  }
+
+}
+
+export default Spectacular;
